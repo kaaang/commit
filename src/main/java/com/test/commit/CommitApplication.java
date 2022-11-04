61efe95e60221fa8fb20c651ddae4decd452bb78
kaaang <kaaang@kakao.com>
2022-11-04 19:40:59 +0900

    Initial commit

---
+++ src/main/java/com/test/commit/CommitApplication.java
@@ -0,0 +1,13 @@
+package com.test.commit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitApplication.class, args);
+    }
+
+}
